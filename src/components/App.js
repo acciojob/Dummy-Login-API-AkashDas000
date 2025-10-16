8909b1c2fbb71dc7b79906bc2f55810b4cf0e693
Akash Das <akashakki1100@gmail.com>
2025-10-16 16:59:52 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,61 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorEmailmsg, setErrorEmailMsg] = useState("");
+  const [errorPasswordmsg, setPasswordMsg] = useState("");
+
+  const data = [
+    {
+      id: 1,
+      name: "ABC",
+      email: "abc@gmail.com",
+      password: "12",
+    },
+    {
+      id: 2,
+      name: "DEF",
+      email: "def@gmail.com",
+      password: "1234",
+    },
+    {
+      id: 3,
+      name: "GHI",
+      email: "ghi@gmail.com",
+      password: "123456",
+    },
+  ];
+
+  return (
+    <div>
+      <input onChange={(e) => setEmail(e.target.value)} id="input-email" />
+      <p id="user-error">{errorEmailmsg}</p>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        id="input-password"
+      />
+      <p id="password-error">{errorPasswordmsg}</p>
+
+      <button
+        onClick={() => {
+          const foundItem = data.find((item) => {
+            return item.email == email;
+          });
+          if (!foundItem) {
+            setErrorEmailMsg("User not found");
+            return;
+          }
+          if (password != foundItem.password) {
+            setPasswordMsg("Password Incorrect");
+          }
+        }}
+        id="submit-form-btn"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default App;
